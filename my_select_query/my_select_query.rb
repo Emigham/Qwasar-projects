ec517fb71d793e1d2f2499b527b2f06b09b819ee
Emigham <41595428+Emigham@users.noreply.github.com>
2023-08-23 00:22:41 +0100

    select query.rb

---
+++ my_select_query/my_select_query.rb
@@ -0,0 +1,24 @@
+require 'csv'
+
+class MySelectQuery
+    @csv_string = []  
+    def initialize(csv) 
+        @csv_string = csv 
+    end
+
+    def where(column_name, criteria)
+        new_array = Array.new
+        lines = @csv_string.split("\n")
+        headers = lines[0].split(',')
+
+        column_name_index = headers.index(column_name)
+        for line in lines do 
+            a = line.parse_csv[column_name_index]
+
+            if (a == criteria)
+                new_array.push(line)
+            end
+        end
+        return new_array
+    end
+end
